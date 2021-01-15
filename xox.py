a188d02ab3ed2b16f06cf49df818a969e676c335
Mert Demirezen <demirezenmert@gmail.com>
2021-01-14 21:41:39 -0500

    updated2

--- xox.py
+++ xox.py
@@ -22,11 +22,11 @@ game  = running
 m_gamer = 'X'   # player mark
 
 def DrawBoard():
-    print(" %c | %c | %c " % (board[1],board[2],board[3]))    
+    print(" %c | %c | %c " % (board[1],board[2],board[3]))
     print("___|___|___")
-    print(" %c | %c | %c " % (board[4],board[5],board[6]))    
+    print(" %c | %c | %c " % (board[4],board[5],board[6]))
     print("___|___|___")
-    print(" %c | %c | %c " % (board[7],board[8],board[9]))    
+    print(" %c | %c | %c " % (board[7],board[8],board[9]))
     print("   |   |   ")
 
 
